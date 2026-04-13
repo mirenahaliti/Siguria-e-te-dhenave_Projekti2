7b2adc78924a28ac6fb167771fa734b223bf9fdc
Mirena Haliti <mirenahalitii@gmail.com>
2026-04-13 12:42:00 +0200

    Delete sp.py

--- sp.py
+++
@@ -1,6 +0,0 @@
-import math
-
-# --- FUNKSIONET NDIHMËSE ---
-def normalize_text(text: str) -> str:
-    """Heq hapesirat per te siguruar qe algoritmi te punoje paster."""
-    return text.upper().replace(" ", "")
